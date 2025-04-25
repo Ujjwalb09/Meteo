d8de3d560632737428f793dd1786d815b603b000
ujjwalb09 <ujjwal.js.dev@gmail.com>
2025-04-25 15:13:19 +0530

    API setup with tanstack and header design

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,29 @@ import Layout from "./components/Layout";
 import { ThemeProvider } from "./context/theme-provider";
 import MeteoDashboard from "./pages/MeteoDashboard";
 import CityPage from "./pages/CityPage";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<MeteoDashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<MeteoDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,39 @@
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <header>Header</header>;
+  const { theme, setTheme } = useTheme();
+
+  const darkMode = theme === "dark";
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-20 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img
+            src="./../../public/meteo-logo.png"
+            alt="meteo logo"
+            className="h-50 w-50"
+          />
+        </Link>
+
+        <div>
+          {/* search */}
+          {/* toggle theme */}
+          <div
+            onClick={() => setTheme(darkMode ? "light" : "dark")}
+            className={`flex items-center cursor-pointer transition-transform duration-500 ${darkMode ? "rotate-180" : "rotate-0"}`}
+          >
+            {darkMode ? (
+              <Sun className="h-6 w-6 text-yellow-500" />
+            ) : (
+              <Moon className="h-6 w-6 text-blue-500" />
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
